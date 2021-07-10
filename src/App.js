dd1dfd6188d26486a07bf64194ea5e32c333dc41
IsleneGomes <islenegomes@gmail.com>
2021-07-10 19:11:16 -0300

    Componentizando

--- src/App.js
+++ src/App.js
@@ -1,50 +1,27 @@
 import { Component } from 'react';
 import './App.css';
+import { Posts } from './components/Posts/index';
+import { loadPosts } from './services/loadPosts';
 
 class App extends Component {
   state = {
     posts: [],
   };
 
-  componentDidMount() {
-    this.loadPosts();
+  async componentDidMount() {
+    await this.loadPost();
   }
 
-  loadPosts = async () => {
-    const postsResponse = fetch("https://jsonplaceholder.typicode.com/posts");
-    const photosResponse = fetch("https://jsonplaceholder.typicode.com/photos")
-    const [posts, photos ] = await Promise.all([postsResponse, photosResponse]);
-    const postsJson = await posts.json();
-    const photosJson = await photos.json();
-
-    const postsAndPhotos = postsJson.map((post, index) => {
-      return { ...post, cover: photosJson[index].url }
-    })
+  loadPost = async () => {
+    const postsAndPhotos = await loadPosts();
     this.setState({ posts: postsAndPhotos });
-  };
-
-  // componentDidUpdate() {
-  // }
-
-  // componentWillUnmount() {
-  // }
+  }
 
   render() {
-    const { posts, cover } = this.state;
+    const { posts } = this.state;
     return (
       <section className="container">
-        <div className="posts">
-          {/* todo array em JS já tem um map disponível que retorna um novo array com as coisas que a gente pedir e para que eu possa retornar mais de um elemento é só colocar () na frente da arrow function, desde que exista um elemento root(pai), pode ser <> </> ou uma div. Todo map exige um key(algo único de cada elemento) no primeiro elemento */}
-          {posts.map((post) => (
-            <div className="post">
-              <div key={post.id} className="post-content">
-                <h1>{post.title}</h1>
-                <img className="post-image" src={post.cover} alt={post.title} />
-                <p>{post.body}</p>
-              </div>
-            </div>
-          ))}
-        </div>
+        <Posts posts={posts} />
       </section>
     );
   }
